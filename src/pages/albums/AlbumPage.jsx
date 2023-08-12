02656dbb6e37c2d0f53c82f2a161e9800cb74758
theinsideshine <educacion.ta@gmail.com>
2023-08-12 08:35:55 -0300

    change for work offline

--- src/pages/albums/AlbumPage.jsx
+++ src/pages/albums/AlbumPage.jsx
@@ -61,86 +61,81 @@ useEffect(() => {
 
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CssBaseline />
-      <main
-        style={{
-          backgroundImage: `url("/public/images/fondo-marmol.jpg")`, // Ajusta la ruta de la imagen aquí
-          // backgroundRepeat: 'repeat',
-          height: '100vh', // Ajusta la altura según tus necesidades
-
-        }}>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'transparent', // Fondo transparente para el Box
-            pt: 8,
-            pb: 6,
+   
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <main
+          style={{
+            backgroundImage: '', // Ajusta la ruta de la imagen aquí
+            // backgroundRepeat: 'repeat',
+            height: '100vh', // Ajusta la altura según tus necesidades
+            display: 'flex',  // Para centrar el contenido verticalmente
+            alignItems: 'center',  // Para centrar el contenido verticalmente
           }}
         >
-          <Container maxWidth="sm">
-            
+          {/* Agregar el marco gris */}
+          <div
+            style={{
+              flex: 1,
+              backgroundColor: 'gray', // Color del marco
+              height: '33vh', // Un tercio de la altura de la pantalla
+              margin: '0 auto', // Centrar horizontalmente
+              borderRadius: '10px', // Borde redondeado opcional
+              padding: '20px', // Espaciado interno opcional
+              display: 'flex', // Para centrar el contenido de la grilla
+              alignItems: 'center', // Para centrar el contenido de la grilla
+            }}
+          >
+            <Container sx={{ py: 8 }} maxWidth="md">
+              {/* Resto de tu código de la grilla */}
+              <Grid container spacing={4}>
+                {albums && albums.length > 0 ? 
+                  (
+                    albums.map((album) => (
+                      <Grid item key={album.id} xs={12} sm={6} md={4}>
+                        <Card
+                          sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                        >
+                          <CardMedia
+                            component="div"
+                            sx={{
+                              // 16:9
+                              pt: '56.25%',
+                            }}
+                            image={`${imageURL}/${album.id}`} // Use the correct URL format
+                            onError={(e) => {
+                              e.target.src = '/public/images/image-not-available.jpg'; // Ruta de la imagen vacía o de respaldo
+                            }}
+                          />
+                          <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography gutterBottom variant="h5" component="h2">
+                              {album.title}
+                            </Typography>
+                            <Typography>
+                              {album.artist}-{album.age}
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small" onClick={() => handleClick(album.id)}>Reproducir</Button>
+                            <Button size="small">Editar</Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    ))
+                  ) : (
+                    <Typography variant="body1">No hay álbumes disponibles.</Typography>
+                  )
+                }
+              </Grid>
+            </Container>
+          </div>
+        </main>
+  
+        {/* Agregar el componente Footer si es necesario */}
+      </ThemeProvider>
+    );
+    
 
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Crear album</Button>
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {albums && albums.length > 0 ? 
-              (
-                albums.map((album) => (
-                  <Grid item key={album.id} xs={12} sm={6} md={4}>
-                    <Card
-                      sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                    >
-                      <CardMedia
-                        component="div"
-                        sx={{
-                          // 16:9
-                          pt: '56.25%',
-                        }}
-                        
-                        image={`${imageURL}/${album.id}`} // Use the correct URL format
-                       
-                        
-                        onError={(e) => {
-                          e.target.src = '/public/images/image-not-available.jpg'; // Ruta de la imagen vacía o de respaldo
-                        }}
-                      />
-                      <CardContent sx={{ flexGrow: 1 }}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {album.title}
-                        </Typography>
-                        <Typography>
-                          {album.artist}-{album.age}
-                        </Typography>
-                      
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" onClick={() => handleClick(album.id)}>Reproducir</Button>
-                        <Button size="small">Editar</Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))
-              ) : (
-                <Typography variant="body1">No hay álbumes disponibles.</Typography>
-              )
-            }
-          </Grid>
-        </Container>
-      </main>
-
-      <Footer />
-
-    </ThemeProvider>
-  );
+      
+ 
 }
